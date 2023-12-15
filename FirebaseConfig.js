440082650ecdcafc89cf51529363b9ff7897aed6
mohitdhiman.zeroit <mohitdhiman.zeroit@gmail.com>
2023-12-15 11:26:56 +0530

    Changes

---
+++ FirebaseConfig.js
@@ -0,0 +1,10 @@
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+// Assuming you have initialized your Firebase app
+const app = initializeApp(yourFirebaseConfig);
+
+// Initialize Firebase Auth with AsyncStorage
+const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
